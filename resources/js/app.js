53e26d96535cf9df976e5d57a3006e65b3ff0d55
ilhamsj <ilhamsaputrajati@gmail.com>
2019-08-24 15:26:05 +0700

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
-require('./bootstrap');
+// require('./bootstrap');
 
 window.Vue = require('vue');
-
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 import ExampleComponent from './components/ExampleComponent.vue'
 import NavbarComponent from './components/NavbarComponent.vue'
+
+Vue.use(VueMaterial)
 
 const app = new Vue({
     el: '#app',
