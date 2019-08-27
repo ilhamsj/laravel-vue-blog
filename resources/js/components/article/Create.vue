957bbca26068ebc828e1530211cb0bc763689619
ilhamsj <ilhamsaputrajati@gmail.com>
2019-08-27 13:19:14 +0700

    insert

--- resources/js/components/article/Create.vue
+++ resources/js/components/article/Create.vue
@@ -3,19 +3,21 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="mb-2">
-                    <router-link :to="{name: 'article.index'}" class="btn btn-primary btn-sm">Back</router-link>
+                    <router-link :to="{name: 'article.index'}">Back</router-link>
                 </div>
-                <div class="card">
-                    <div class="card-header">Article Index</div>
-
-                    <div class="card-body">
-                        <form @submit.prevent="addPost" >
-                            <input type="text" v-model="post.title">
-                            <input type="text" v-model="post.content">
-                            <button>Create</button>
-                        </form>
+                <form @submit.prevent="addPost" >
+                    <div class="form-group">
+                        <label for="">Title</label>
+                        <input type="text" v-model="post.title" class="form-control">
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label for="">Content</label>
+                        <textarea v-model="post.content" cols="30" rows="10" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-primary">Create</button >
+                    </div>
+                </form>
             </div>
         </div>
     </div>
