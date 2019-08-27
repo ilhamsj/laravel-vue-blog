44eefb287dc046d2f3123701532f95efd7ef478b
ilhamsj <ilhamsaputrajati@gmail.com>
2019-08-27 11:41:06 +0700

    secure

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{csrf_token()}}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>{{ env('app_env') }}</title>
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
 </head>
 <body>
     <div id="app">
         <router-view name="home"></router-view>
     </div>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ secure_asset('js/app.js') }}"></script>
 </body>
 </html>
