75ebce941dd14014e66360eeaf1c8e28934be4cd
ilhamsj <ilhamsaputrajati@gmail.com>
2019-11-27 11:52:08 +0700

    secure

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{env('app_name')}}</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css')}}">
 </head>
 <body>
     <div id="app">
         <router-view></router-view>
     </div>
-    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{secure_asset('js/app.js')}}"></script>
 </body>
 </html>
